6ccf22c49e10716324871a19f0dae0586cbe8c68
Victor Teo <victor.teo@fillmula.com>
2023-10-10 02:48:40 +0800

    remove teo parser temporarily

--- src/pipeline/pipeline.rs
+++ src/pipeline/pipeline.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use crate::pipeline::item::BoundedItem;
 
 #[derive(Debug)]
@@ -7,5 +8,24 @@ pub struct Pipeline {
 
 impl Pipeline {
 
+}
 
+impl Display for Pipeline {
+
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for (index, item) in self.items.iter().enumerate() {
+            if index == 0 {
+                f.write_str("$")?;
+            } else {
+                f.write_str(".")?;
+            }
+            f.write_str(&item.path.join("."))?;
+            if !item.arguments.is_empty() {
+                f.write_str("(")?;
+                //f.write_str(&item.arguments.iter().map(|(k, v)| format!("{k}: {}", v)).join(", "))?;
+                f.write_str(")")?;
+            }
+        }
+        Ok(())
+    }
 }
