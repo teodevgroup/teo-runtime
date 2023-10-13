f7bfac19b5efeec457b5ccf51a413c4551b542a0
XiaYuan Wu <i2256g03c3@163.com>
2023-10-13 20:04:22 +0800

    without pipeline of oneOf on value operation

--- src/stdlib/pipeline_items/value.rs
+++ src/stdlib/pipeline_items/value.rs
@@ -94,4 +94,55 @@ pub(in crate::stdlib) fn load_pipeline_value_items(namespace: &mut Namespace) {
             Err(Error::new("value is equal to rhs"))?
         }
     });
+
+    namespace.define_pipeline_item("isNull", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("isNull")?;
+        if !input.is_null() {
+            Err(Error::new("value is not Null"))?
+        }
+        Ok(ctx.value().clone())
+    });
+
+    namespace.define_pipeline_item("exists", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("exists")?;
+        if input.is_null() {
+            Err(Error::new("value does not exist"))?
+        }
+        Ok(ctx.value().clone())
+    });
+
+    namespace.define_pipeline_item("isTrue", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("isTrue")?;
+        if input.as_bool().is_some(){
+            Ok(ctx.value().clone())
+        } else {
+            Err(Error::new("value is equal to rhs"))?
+        }
+    });
+
+    namespace.define_pipeline_item("isFalse", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("isFalse")?;
+        if !input.as_bool().is_some(){
+            Ok(ctx.value().clone())
+        } else {
+            Err(Error::new("value is equal to rhs"))?
+        }
+    });
+
+    namespace.define_pipeline_item("oneOf", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("oneOf")?;
+        let arg_object = &ctx.resolve_pipeline(
+            args.get_object("value").err_prefix("oneOf(value)")?,
+            "oneOf(value)",
+        ).await?;
+        let arg: &Value = arg_object.try_into_err_prefix("oneOf(value)")?;
+        let list = arg.as_vec().unwrap();
+        if list.iter().find(|item| {
+            **item == input
+        }).is_some() {
+            Ok(ctx.value().clone())
+        } else {
+            Err(Error::new("value is equal to rhs"))?
+        }
+    });
 }
