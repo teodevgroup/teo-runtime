402b818e210e0be9187b73c51f141d7e27de08cf
XiaYuan Wu <i2256g03c3@163.com>
2023-10-13 14:43:05 +0800

    add woWordCase pipeline

--- src/stdlib/pipeline_items/string/transform.rs
+++ src/stdlib/pipeline_items/string/transform.rs
@@ -11,11 +11,8 @@ use teo_teon::Value;
 
 pub(in crate::stdlib) fn load_pipeline_string_transform_items(namespace: &mut Namespace) {
 
-    // namespace.define_pipeline_item("hasPrefix", |args: Arguments, ctx: Ctx| async move {
-    //     let input: &str = ctx.value().try_into_err_prefix("hasPrefix")?;
-    //     Ok(
-    //         // str => 
-    //         Object::from(Value::String(to_word_case(s)))
-    //     )
-    // });
+    namespace.define_pipeline_item("toWordCase", |args: Arguments, ctx: Ctx| async move {
+        let input: &str = ctx.value().try_into_err_prefix("toWordCase")?;
+        Ok( Object::from(Value::String(to_word_case(input))))
+    });
 }
