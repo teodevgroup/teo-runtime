ebac8a6ffc7cc03acb9a3e3d8f55cf5a79a8a570
XiaYuan Wu <i2256g03c3@163.com>
2023-10-13 14:38:08 +0800

    regexMatch not completed

--- src/stdlib/pipeline_items/string/transform.rs
+++ src/stdlib/pipeline_items/string/transform.rs
@@ -1,5 +1,21 @@
 use crate::namespace::Namespace;
+use crate::arguments::Arguments;
+use crate::pipeline::Ctx;
+use crate::error::Error;
+use crate::result::ResultExt;
+
+use crate::object::Object;
+use inflector::cases::wordcase::to_word_case;
+use teo_teon::Value;
+
 
 pub(in crate::stdlib) fn load_pipeline_string_transform_items(namespace: &mut Namespace) {
 
+    // namespace.define_pipeline_item("hasPrefix", |args: Arguments, ctx: Ctx| async move {
+    //     let input: &str = ctx.value().try_into_err_prefix("hasPrefix")?;
+    //     Ok(
+    //         // str => 
+    //         Object::from(Value::String(to_word_case(s)))
+    //     )
+    // });
 }
