20524896a8d49ed7da06f48a2c3f423079ef53a1
XiaYuan Wu <i2256g03c3@163.com>
2023-10-14 04:23:22 +0800

    complete array

--- src/stdlib/pipeline_items/array.rs
+++ src/stdlib/pipeline_items/array.rs
@@ -31,61 +31,71 @@ pub(in crate::stdlib) fn load_pipeline_array_items(namespace: &mut Namespace) {
         }
     });
 
-    // namespace.define_pipeline_item("append", |args: Arguments, ctx: Ctx| async move {
-    //     let input: &Value = ctx.value().try_into_err_prefix("append")?;
-    //     let array_object = ctx.resolve_pipeline(
-    //         args.get_object("value").err_prefix("append(value)")?,
-    //         "append(value)" ,
-    //     ).await?;
-    //     let arg_array: &Value = array_object.try_into_err_prefix("append(value)")?;
-    //     match input{
-    //         Value::String(s) => {
-    //             match arg_array {
-    //                 Value::String(a) => Ok(Object::from(s.to_owned() + &a)),
-    //                 _ => Err(Error::new("Argument can't match"))?
-    //             }
-    //         }
-    //         Value::Array(v) => {
-    //             let mut v = v.clone();
-    //             match arg_array {
-    //                 Value::Array(a) => { 
-    //                     for item in a.iter() {
-    //                         v.push(item.clone()) ;
-    //                     }
-    //                     Ok(Object::from(  v  )) 
-    //                 }
-    //                 _ => Err(Error::new("Argument can't match"))?
-    //             }
-    //         }
-    //         _ => Err(Error::new("input is not array"))?
-    //     }
-    // });
-
-    // namespace.define_pipeline_item("prepend", |args: Arguments, ctx: Ctx| async move {
-    //     let input: &str = ctx.value().try_into_err_prefix("prepend")?;
-    //     let array_object = ctx.resolve_pipeline( 
-    //         args.get_object("value").err_prefix("prepend(value)"),
-    //         "prepend(value)" ,
-    //     ).await?;
-    //     let mut arg_array: Vec = array_object.get("value").err_prefix("prepend(value)").clone();
-    //     arg_array.push(input);
-    //     Ok(Object::from(arg_array))
-    // });
+    namespace.define_pipeline_item("prepend", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("prepend")?;
+        let arg_object = ctx.resolve_pipeline( 
+            args.get_object("value").err_prefix("prepend(value)")?,
+            "prepend(value)" ,
+        ).await?;
+        let mut arg: Value = arg_object.try_into_err_prefix("prepend(value)")?;
+        match input {
+            Value::String(s) => {
+                if !arg.is_string() {
+                    Err(Error::new("preend(value): value is not string"))?
+                }
+                Ok(Object::from(arg.as_str().unwrap().to_owned() + &s.clone()))
+            },
+            Value::Array(v) => {
+                let mut new_array = v.clone();
+                new_array.insert(0, arg);
+                Ok(Object::from(new_array))
+            },
+            _ => Err(Error::new("preend: input is not array or string"))
+        }
+    });
 
     namespace.define_pipeline_item("getLength", |args: Arguments, ctx: Ctx| async move {
         let input: &Value = ctx.value().try_into_err_prefix("getLength")?;
         Ok( match input {
-            Value::String(s) =>   Object::from(s.len() as i32),
-            Value::Array(v) =>    Object::from(v.len() as i32),
+            Value::String(s) => Object::from(s.len() as i32),
+            Value::Array(v) => Object::from(v.len() as i32),
             _ => Err(Error::new("getLength: input is not array or string"))?
         })
     });
 
-    // namespace.define_pipeline_item("hasLength", |args: Arguments, ctx: Ctx| async move {
-    //     let input: &Value = ctx.value().try_into_err_prefix("hasLength")?;
-    //     match input {
-    //     }
-    // });
+    namespace.define_pipeline_item("hasLength", |args: Arguments, ctx: Ctx| async move {
+        let input: &Value = ctx.value().try_into_err_prefix("hasLength")?;
+        let (lower , upper , closed) = if input.is_any_number(){
+            let n = input.to_usize().unwrap();
+            (n ,n ,true)
+        } else if input.is_range() {
+            let r = input.as_range().unwrap();
+            let start = r.start.to_usize().unwrap();
+            let end = r.end.to_usize().unwrap();
+            (start, end, r.closed)
+        } else {
+            unreachable!()
+        };
+        let len = match input{
+            Value::String(s) => s.len(),
+            Value::Array(v) => v.len(),
+            _ => Err(Error::new("reverse: input is not array or string"))?
+        };
+        if len < lower {
+            Err(Error::new("Value length is less than {lower}."))?
+        }
+        if closed { if len > upper {
+                Err(Error::new("Value length is less than {lower}."))?
+            } else {
+                Ok(Object::from(input.as_int().unwrap()))
+            }
+        } else { if len >= upper {
+                Err(Error::new("Value length is greater than or equal to {upper}."))?
+            } else {
+                Ok(Object::from(input.as_int().unwrap()))
+            }
+        }
+    });
 
     namespace.define_pipeline_item("reverse", |args: Arguments, ctx: Ctx| async move {
         let input: &Value = ctx.value().try_into_err_prefix("reverse")?;
