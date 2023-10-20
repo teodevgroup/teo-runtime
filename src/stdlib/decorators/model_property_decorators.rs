dc6cb2f6b1e5d9da1525fbf58c8f025a20cd65dd
Victor Teo <victor.teo@fillmula.com>
2023-10-21 04:37:16 +0800

    property decorators

--- src/stdlib/decorators/model_property_decorators.rs
+++ src/stdlib/decorators/model_property_decorators.rs
@@ -1,27 +1,36 @@
+use teo_teon::types::enum_variant::EnumVariant;
+use teo_teon::Value;
+use teo_result::Result;
 use crate::namespace::Namespace;
+use crate::pipeline::pipeline::Pipeline;
 
 pub(in crate::stdlib) fn load_model_property_decorators(namespace: &mut Namespace) {
 
     namespace.define_model_property_decorator("getter", |arguments, property| {
+        let pipeline: Pipeline = arguments.get("pipeline")?;
+        property.getter = Some(pipeline);
         Ok(())
     });
 
-    // /// @name Getter
-    // /// Define a property with getter
-    // declare unique model property decorator getter(pipeline?: Pipeline<Self, ThisFieldType>)
-    //
-    // /// @name Setter
-    // /// Define a property with setter
-    // declare unique model property decorator setter(pipeline?: Pipeline<Self, Ignored>)
-    //
-    // /// @name Cache
-    // /// Define a cached property, a cached property is saved into the database
-    // declare unique model property decorator cached
-    //
-    // /// @name Dependencies
-    // /// Define dependencies for a cached property
-    // declare unique model property decorator deps(deps?: ModelScalarFieldsWithoutVirtuals<Self> | ModelScalarFieldsWithoutVirtuals<Self>[])
-    //
+    namespace.define_model_property_decorator("setter", |arguments, property| {
+        let pipeline: Pipeline = arguments.get("pipeline")?;
+        property.setter = Some(pipeline);
+        Ok(())
+    });
+
+    namespace.define_model_property_decorator("cached", |arguments, property| {
+        property.cached = true;
+        Ok(())
+    });
+
+    namespace.define_model_property_decorator("deps", |arguments, property| {
+        let deps: Value = arguments.get("deps")?;
+        let deps: Vec<EnumVariant> = deps.into_vec()?;
+        let deps: Vec<String> = deps.iter().map(|f| f.clone().into_string()).collect::<Result<Vec<_>>>()?;
+        property.dependencies = deps;
+        Ok(())
+    });
+
     // /// @name Index
     // /// Define index for this cached property
     // declare unique model property decorator index(sort: Sort?, length: Int?, map: String?)
@@ -29,5 +38,4 @@ pub(in crate::stdlib) fn load_model_property_decorators(namespace: &mut Namespac
     // /// @name Unique
     // /// Define unique index for this cached property
     // declare unique model property decorator unique(sort: Sort?, length: Int?, map: String?)
-
 }
