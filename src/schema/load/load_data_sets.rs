a1c3be5cd32881b6e4a526ba7b54ea17d5a03475
Victor Teo <victor.teo@fillmula.com>
2023-11-05 02:57:53 +0800

    update load data set

--- src/schema/load/load_data_sets.rs
+++ src/schema/load/load_data_sets.rs
@@ -8,34 +8,35 @@ use crate::schema::fetch::fetchers::fetch_literals::fetch_dictionary_literal;
 pub fn load_data_sets(namespace: &Namespace, names: Option<&Vec<String>>, all: bool, schema: &Schema) -> Result<Vec<DataSet>> {
     let mut result: Vec<DataSet> = vec![];
     for schema_data_set in schema.data_sets() {
-        if result.iter().find(|d| d.name == schema_data_set.string_path).is_none() {
-            result.push(DataSet {
-                notrack: false,
-                autoseed: false,
-                name: schema_data_set.string_path.clone(),
-                groups: vec![]
-            });
-        }
-        let data_set = result.iter_mut().find(|d| d.name == schema_data_set.string_path).unwrap();
-        data_set.notrack = schema_data_set.notrack;
-        data_set.autoseed = schema_data_set.auto_seed;
-        for schema_group in &schema_data_set.groups {
-            if data_set.groups.iter().find(|g| g.name == schema_group.resolved().model_string_path).is_none() {
-                data_set.groups.push(Group {
-                    name: schema_group.resolved().model_string_path.clone(),
-                    records: vec![]
+        if all || (names.is_some() && names.unwrap().contains(&schema_data_set.string_path.join("."))) || (names.is_none() && schema_data_set.auto_seed) {
+            if result.iter().find(|d| d.name == schema_data_set.string_path).is_none() {
+                result.push(DataSet {
+                    notrack: false,
+                    autoseed: false,
+                    name: schema_data_set.string_path.clone(),
+                    groups: vec![]
                 });
             }
-            let group = data_set.groups.iter_mut().find(|g| g.name == schema_group.resolved().model_string_path).unwrap();
-            for schema_record in &schema_group.records {
-                let record = Record {
-                    name: schema_record.identifier.name().to_owned(),
-                    value: fetch_dictionary_literal(&schema_record.dictionary, schema, schema_record, &Type::Undetermined, namespace)?.as_teon().unwrap().clone(),
-                };
-                group.records.push(record);
+            let data_set = result.iter_mut().find(|d| d.name == schema_data_set.string_path).unwrap();
+            data_set.notrack = schema_data_set.notrack;
+            data_set.autoseed = schema_data_set.auto_seed;
+            for schema_group in &schema_data_set.groups {
+                if data_set.groups.iter().find(|g| g.name == schema_group.resolved().model_string_path).is_none() {
+                    data_set.groups.push(Group {
+                        name: schema_group.resolved().model_string_path.clone(),
+                        records: vec![]
+                    });
+                }
+                let group = data_set.groups.iter_mut().find(|g| g.name == schema_group.resolved().model_string_path).unwrap();
+                for schema_record in &schema_group.records {
+                    let record = Record {
+                        name: schema_record.identifier.name().to_owned(),
+                        value: fetch_dictionary_literal(&schema_record.dictionary, schema, schema_record, &Type::Undetermined, namespace)?.as_teon().unwrap().clone(),
+                    };
+                    group.records.push(record);
+                }
             }
         }
-
     }
     Ok(result)
 }
