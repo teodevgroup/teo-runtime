f292572768d5f29f9b06d2fe8eb10b584123b2c8
Victor Teo <victor.teo@fillmula.com>
2023-11-21 11:33:44 +0800

    fetch unit object

--- src/schema/fetch/fetchers/fetch_identifier.rs
+++ src/schema/fetch/fetchers/fetch_identifier.rs
@@ -4,8 +4,9 @@ use teo_parser::traits::info_provider::InfoProvider;
 use teo_parser::ast::schema::Schema;
 use teo_parser::ast::node::Node;
 use teo_parser::ast::reference_space::ReferenceSpace;
+use teo_parser::expr::ExprInfo;
 use teo_parser::r#type::Type;
-use teo_parser::search::search_identifier_path::search_identifier_path_names_with_filter_to_top;
+use teo_parser::search::search_identifier_path::{search_identifier_path_names_with_filter_to_expr_info, search_identifier_path_names_with_filter_to_top};
 use teo_parser::traits::named_identifiable::NamedIdentifiable;
 use teo_parser::utils::top_filter::top_filter_for_reference_type;
 use teo_result::{Error, Result};
@@ -15,7 +16,7 @@ use crate::object::Object;
 use crate::schema::fetch::fetch_expression::fetch_expression;
 
 pub fn fetch_identifier<I>(identifier: &Identifier, schema: &Schema, info_provider: &I, expect: &Type, namespace: &Namespace) -> Result<Object> where I: InfoProvider {
-    let top = fetch_identifier_to_node(identifier, schema, info_provider, expect, namespace, &top_filter_for_reference_type(ReferenceSpace::Default))?;
+    let top = fetch_identifier_to_node(identifier, schema, info_provider, expect,  &top_filter_for_reference_type(ReferenceSpace::Default))?;
     match top {
         Node::Config(c) => Err(Error::new("cannot resolve")),
         Node::ConstantDeclaration(c) => fetch_expression(c.expression(), schema, info_provider, expect, namespace),
@@ -28,8 +29,19 @@ pub fn fetch_identifier<I>(identifier: &Identifier, schema: &Schema, info_provid
     }
 }
 
-pub fn fetch_identifier_to_node<'a, I>(identifier: &Identifier, schema: &'a Schema, info_provider: &I, _expect: &Type, namespace: &Namespace, filter: &Arc<dyn Fn(&Node) -> bool>) -> Result<&'a Node> where I: InfoProvider {
+pub fn fetch_identifier_to_node<'a, I>(identifier: &Identifier, schema: &'a Schema, info_provider: &I, _expect: &Type, filter: &Arc<dyn Fn(&Node) -> bool>) -> Result<&'a Node> where I: InfoProvider {
     Ok(search_identifier_path_names_with_filter_to_top(
+        &vec![identifier.name()],
+        schema,
+        schema.source(info_provider.source_id()).unwrap(),
+        &info_provider.namespace_str_path(),
+        filter,
+        info_provider.availability(),
+    ).unwrap())
+}
+
+pub fn fetch_identifier_to_expr_info<'a, I>(identifier: &Identifier, schema: &'a Schema, info_provider: &I, _expect: &Type, filter: &Arc<dyn Fn(&Node) -> bool>) -> Result<ExprInfo> where I: InfoProvider {
+    Ok(search_identifier_path_names_with_filter_to_expr_info(
         &vec![identifier.name()],
         schema,
         schema.source(info_provider.source_id()).unwrap(),
--- src/schema/fetch/fetchers/fetch_unit.rs
+++ src/schema/fetch/fetchers/fetch_unit.rs
@@ -1,40 +1,51 @@
+use maplit::btreemap;
 use teo_parser::ast::expression::{Expression, ExpressionKind};
 use teo_parser::ast::reference_space::ReferenceSpace;
 use teo_parser::ast::unit::Unit;
 use teo_parser::traits::info_provider::InfoProvider;
 use teo_parser::ast::schema::Schema;
+use teo_parser::expr::{ExprInfo, ReferenceInfo, ReferenceType};
+use teo_parser::r#type::reference::Reference;
 use teo_parser::r#type::Type;
+use teo_parser::traits::identifiable::Identifiable;
 use teo_parser::traits::named_identifiable::NamedIdentifiable;
 use teo_parser::traits::resolved::Resolve;
 use teo_parser::utils::top_filter::top_filter_for_reference_type;
 use teo_result::{Error, Result};
 use teo_teon::types::enum_variant::EnumVariant;
 use teo_teon::Value;
+use crate::arguments::Arguments;
 use crate::namespace::Namespace;
 use crate::object::Object;
 use crate::object::traits::PrimitiveStruct;
 use crate::schema::fetch::fetch_argument_list::fetch_argument_list;
 use crate::schema::fetch::fetch_expression::fetch_expression;
+use crate::schema::fetch::fetchers::fetch_identifier::{fetch_identifier_to_expr_info, fetch_identifier_to_node};
 
 #[derive(Debug)]
 pub(super) enum UnitFetchResult {
-    Reference(Vec<usize>),
+    Reference(ReferenceInfo, Option<Object>),
     Object(Object),
 }
 
 impl UnitFetchResult {
 
-    pub(super) fn into_object(self, schema: &Schema) -> Result<Object> {
+    pub(super) fn into_object(self, schema: &Schema, expect: &Type) -> Result<Object> {
         match self {
             UnitFetchResult::Object(o) => Ok(o),
-            UnitFetchResult::Reference(r) => {
-                let top = schema.find_top_by_path(&r).unwrap();
-                if top.is_model() {
-                    Ok(Object::from(Value::from(top.as_model().unwrap().string_path().clone())))
-                } else if top.is_data_set() {
-                    Ok(Object::from(Value::from(top.as_data_set().unwrap().string_path().clone())))
-                } else {
-                    Err(Error::new("cannot convert reference into object"))?
+            UnitFetchResult::Reference(reference_info, _) => {
+                match reference_info.r#type() {
+                    ReferenceType::Model => Ok(Object::from(Value::from(reference_info.reference().string_path().clone()))),
+                    ReferenceType::DataSet => Ok(Object::from(Value::from(reference_info.reference().string_path().clone()))),
+                    ReferenceType::ModelField => Ok(Object::from(Value::EnumVariant(EnumVariant {
+                        value: reference_info.reference.string_path().last().unwrap().clone(),
+                        args: None,
+                    }))),
+                    ReferenceType::EnumMember => Ok(Object::from(Value::EnumVariant(EnumVariant {
+                        value: reference_info.reference.string_path().last().unwrap().clone(),
+                        args: None,
+                    }))),
+                    _ => Err(Error::new("cannot convert reference into object"))?,
                 }
             }
         }
@@ -54,9 +65,20 @@ pub fn fetch_unit<I>(unit: &Unit, schema: &Schema, info_provider: &I, expect: &T
     }
 }
 
-fn fetch_current_item_for_unit<I>(current: Option<UnitFetchResult>, item: &Expression, schema: &Schema, info_provider: &I, expect: &Type, namespace: &Namespace) -> Result<UnitFetchResult> where I: InfoProvider {
+fn fetch_current_item_for_unit<I>(current: Option<UnitFetchResult>, expression: &Expression, schema: &Schema, info_provider: &I, expect: &Type, namespace: &Namespace) -> Result<UnitFetchResult> where I: InfoProvider {
     let Some(current) = current else {
-
+        let expected = Type::Undetermined;
+        return Ok(if let Some(identifier) = expression.kind.as_identifier() {
+            let top = fetch_identifier_to_node(identifier, schema, info_provider, &expected, &top_filter_for_reference_type(ReferenceSpace::Default))?;
+            let expr_info = fetch_identifier_to_expr_info(identifier, schema, info_provider, &expected,  &top_filter_for_reference_type(ReferenceSpace::Default))?;
+            if let Some(constant) = top.as_constant_declaration() {
+                UnitFetchResult::Object(fetch_expression(constant.expression(), schema, info_provider, &expected, namespace)?)
+            } else {
+                UnitFetchResult::Reference(expr_info.reference_info().unwrap().clone(), None)
+            }
+        } else {
+            UnitFetchResult::Object(fetch_expression(expression, schema, info_provider, &expected, namespace)?)
+        });
     };
     match current {
         UnitFetchResult::Object(current_value) => {
@@ -67,22 +89,32 @@ fn fetch_current_item_for_unit<I>(current: Option<UnitFetchResult>, item: &Expre
             } else {
                 Err(Error::new("not supported"))?
             };
-            match &item.kind {
-                ExpressionKind::Identifier(_) => {
-                    Err(Error::new("not implemented"))
+            let r#struct = namespace.struct_at_path(&path).unwrap();
+            let struct_definition = schema.std_source().find_node_by_string_path(&path).unwrap().as_struct_declaration().unwrap();
+            match &expression.kind {
+                ExpressionKind::Identifier(identifier) => {
+                    let instance_function = r#struct.function(identifier.name()).unwrap();
+                    let instance_function_definition = struct_definition.instance_function(identifier.name()).unwrap();
+                    UnitFetchResult::Reference(ReferenceInfo::new(
+                        ReferenceType::StructInstanceFunction,
+                        Reference::new(instance_function_definition.path().clone(), instance_function_definition.string_path().clone()),
+                        None,
+                    ), Some(current_value))
                 }
                 ExpressionKind::Subscript(subscript) => {
-                    let r#struct = namespace.struct_at_path(&path).unwrap();
-                    let function = r#struct.function("subscript").unwrap();
-                    // let arguments = fetch_argument_list(&call.argument_list, schema, info_provider, namespace)?;
-                    // let result = function.body.call(current_value.clone(), arguments)?;
-                    // return Ok(UnitFetchResult::Object(result));
-                    unreachable!()
+                    let instance_function = r#struct.function("subscript").unwrap();
+                    let instance_function_definition = struct_definition.instance_function("subscript").unwrap();
+                    let only_argument_declaration = instance_function_definition.argument_list_declaration().argument_declarations().next().unwrap();
+                    let expected = only_argument_declaration.type_expr().resolved();
+                    let only_argument = fetch_expression(subscript.expression(), schema, info_provider, expected, namespace)?;
+                    let only_argument_name = only_argument_declaration.name().name();
+                    let arguments = Arguments::new(btreemap! {only_argument_name.to_owned() => only_argument});
+                    return Ok(UnitFetchResult::Object(instance_function.body.call(current_value, arguments)?));
                 }
                 _ => unreachable!(),
             }
         }
-        UnitFetchResult::Reference(path) => {
+        UnitFetchResult::Reference(reference_info, this) => {
             match schema.find_top_by_path(path).unwrap() {
                 Top::StructDeclaration(struct_declaration) => {
                     let struct_object = Type::StructObject(struct_declaration.path.clone(), struct_declaration.string_path.clone());
