d83036f00972c1abea9fefbe64739c83609fb5e1
Victor Teo <victor.teo@fillmula.com>
2023-12-22 11:29:58 +0800

    create placeholder handler

--- src/schema/load/load_handler.rs
+++ src/schema/load/load_handler.rs
@@ -1,3 +1,4 @@
+use teo_parser::ast::handler::HandlerInputFormat;
 use teo_parser::ast::schema::Schema;
 use teo_parser::diagnostics::diagnostics::Diagnostics;
 use teo_parser::r#type::Type;
@@ -5,39 +6,61 @@ use teo_parser::traits::info_provider::InfoProvider;
 use teo_parser::traits::named_identifiable::NamedIdentifiable;
 use teo_parser::traits::resolved::Resolve;
 use teo_result::Result;
+use crate::handler::Handler;
 use crate::handler::handler::Method;
 use crate::namespace::Namespace;
+use crate::path::Error;
+use crate::request;
+use crate::response::Response;
 use crate::schema::fetch::fetch_decorator_arguments::fetch_decorator_arguments;
+use crate::utils::next_path;
 
 pub fn load_handler(main_namespace: &mut Namespace, schema: &Schema, handler_declaration: &teo_parser::ast::handler::HandlerDeclaration, diagnostics: &mut Diagnostics) -> Result<()> {
-    if let Some(mut handler) = main_namespace.handler_at_path(&handler_declaration.str_path()).cloned() {
-        handler.format = handler_declaration.input_format;
-        handler.nonapi = handler_declaration.nonapi;
-        handler.input_type = handler_declaration.input_type().map_or(Type::Any, |t| t.resolved().clone());
-        handler.output_type = handler_declaration.output_type().resolved().clone();
-        for decorator in handler_declaration.decorators() {
-            let decorator_declaration = schema.find_top_by_path(decorator.resolved()).unwrap().as_decorator_declaration().unwrap();
-            if let Some(decorator_implementation) = main_namespace.handler_decorator_at_path(&decorator_declaration.str_path()) {
-                let args = fetch_decorator_arguments(decorator, schema, handler_declaration, main_namespace)?;
-                (decorator_implementation.call)(args, &mut handler)?;
-            }
+    let mut handler = if let Some(handler) = main_namespace.handler_at_path(&handler_declaration.str_path()).cloned() {
+        handler
+    } else {
+        // just load a default empty one, for generating interfaces
+        Handler {
+            input_type: Type::Undetermined,
+            output_type: Type::Undetermined,
+            nonapi: false,
+            format: HandlerInputFormat::Json,
+            path: handler_declaration.string_path().clone(),
+            ignore_prefix: false,
+            method: Method::Post,
+            interface: None,
+            url: None,
+            call: Box::leak(Box::new(|ctx: request::Ctx| async {
+                Err(Error::not_found_message_only())
+            })),
         }
-        if (handler.method != Method::Post) || handler.url.is_some() {
-            let parent_string_path = handler_declaration.parent_string_path();
-            main_namespace.handler_map.add_record(
-                &handler_declaration.namespace_str_path(),
-                if handler_declaration.namespace_skip() == 2 {
-                    Some(parent_string_path.last().unwrap().as_str())
-                } else {
-                    None
-                },
-                handler_declaration.name(),
-                handler.method,
-                handler.url.as_ref().map(|u| u.as_str()),
-                handler.ignore_prefix,
-            );
+    };
+    handler.format = handler_declaration.input_format;
+    handler.nonapi = handler_declaration.nonapi;
+    handler.input_type = handler_declaration.input_type().map_or(Type::Any, |t| t.resolved().clone());
+    handler.output_type = handler_declaration.output_type().resolved().clone();
+    for decorator in handler_declaration.decorators() {
+        let decorator_declaration = schema.find_top_by_path(decorator.resolved()).unwrap().as_decorator_declaration().unwrap();
+        if let Some(decorator_implementation) = main_namespace.handler_decorator_at_path(&decorator_declaration.str_path()) {
+            let args = fetch_decorator_arguments(decorator, schema, handler_declaration, main_namespace)?;
+            (decorator_implementation.call)(args, &mut handler)?;
         }
-        main_namespace.replace_handler_at_path(&handler_declaration.str_path(), handler, handler_declaration.inside_group);
     }
+    if (handler.method != Method::Post) || handler.url.is_some() {
+        let parent_string_path = handler_declaration.parent_string_path();
+        main_namespace.handler_map.add_record(
+            &handler_declaration.namespace_str_path(),
+            if handler_declaration.namespace_skip() == 2 {
+                Some(parent_string_path.last().unwrap().as_str())
+            } else {
+                None
+            },
+            handler_declaration.name(),
+            handler.method,
+            handler.url.as_ref().map(|u| u.as_str()),
+            handler.ignore_prefix,
+        );
+    }
+    main_namespace.replace_handler_at_path(&handler_declaration.str_path(), handler, handler_declaration.inside_group);
     Ok(())
 }
