fef755ae73fdd5d347e1678f6c84a3a4ec8aeaa3
Victor Teo <victor.teo@fillmula.com>
2024-01-08 17:46:53 +0800

    add meta map to path error

--- src/path/error.rs
+++ src/path/error.rs
@@ -1,7 +1,11 @@
+use std::any::Any;
+use std::collections::BTreeMap;
 use std::fmt::{Display, Formatter};
+use std::sync::Arc;
 use indexmap::{IndexMap, indexmap};
 use itertools::Itertools;
 use key_path::KeyPath;
+use maplit::btreemap;
 use teo_teon::Value;
 
 #[derive(Debug)]
@@ -10,9 +14,18 @@ pub struct Error {
     pub message: String,
     pub fields: Option<IndexMap<String, String>>,
     pub code: i32,
+    pub meta_map: BTreeMap<String, Arc<dyn Any + Send + Sync>>,
 }
 
 impl Error {
+
+    pub fn insert_meta<T: 'static + Send + Sync>(&mut self, key: impl Into<String>, val: T) {
+        self.meta_map.insert(key.into(), Arc::new(val));
+    }
+
+    pub fn get_meta<T: 'static + Send>(&self, key: &str) -> Option<&T> {
+        self.meta_map.get(key).and_then(|boxed| boxed.downcast_ref())
+    }
 
     pub fn message(&self) -> &str {
         self.message.as_str()
@@ -25,7 +38,8 @@ impl Error {
             fields: Some(indexmap!{
                 path.to_string() => message.into()
             }),
-            code: 400
+            code: 400,
+            meta_map: btreemap! {},
         }
     }
 
@@ -34,7 +48,8 @@ impl Error {
             title: "ValueError",
             message: message.into(),
                         fields: None,
-            code: 400
+            code: 400,
+            meta_map: btreemap! {},
         }
     }
 
@@ -45,7 +60,8 @@ impl Error {
             fields: Some(indexmap! {
                 path.to_string() => format!("value violates '{}' constraint", constraint.as_ref())
             }),
-            code: 400
+            code: 400,
+            meta_map: btreemap! {},
         }
     }
 
@@ -56,7 +72,8 @@ impl Error {
             fields: Some(indexmap! {
                 path.to_string() => message.into()
             }),
-            code: 500
+            code: 500,
+            meta_map: btreemap! {},
         }
     }
 
@@ -66,6 +83,7 @@ impl Error {
             message: message.into(),
             fields: None,
             code: 500,
+            meta_map: btreemap! {},
         }
     }
 
@@ -76,7 +94,8 @@ impl Error {
             fields: Some(indexmap!{
                 path.to_string() => "not found".to_owned()
             }),
-            code: 404
+            code: 404,
+            meta_map: btreemap! {},
         }
     }
 
@@ -85,7 +104,8 @@ impl Error {
             title: "NotFound",
             message: "not found".to_owned(),
             fields: None,
-            code: 404
+            code: 404,
+            meta_map: btreemap! {},
         }
     }
 
@@ -96,7 +116,8 @@ impl Error {
             fields: Some(indexmap! {
                 path.to_string() => message.into()
             }),
-            code: 401
+            code: 401,
+            meta_map: btreemap! {},
         }
     }
 
@@ -105,7 +126,8 @@ impl Error {
             title: "Unauthorized",
             message: message.into(),
             fields: None,
-            code: 401
+            code: 401,
+            meta_map: btreemap! {},
         }
     }
 }
@@ -148,14 +170,16 @@ impl IntoPathedValueError for teo_result::Error {
 impl From<&teo_result::Error> for Error {
 
     fn from(value: &teo_result::Error) -> Self {
-        Self::internal_server_error_message_only(value.message.clone())
+        let mut result = Self::internal_server_error_message_only(value.message.clone());
+        result.meta_map = value.meta_map.clone();
+        result
     }
 }
 
 impl From<teo_result::Error> for Error {
 
     fn from(value: teo_result::Error) -> Self {
-        Self::internal_server_error_message_only(value.message)
+        Self::from(&value)
     }
 }
 
@@ -174,7 +198,9 @@ impl From<&Error> for teo_result::Error {
         } else {
             value.message.clone()
         };
-        teo_result::Error::new(message)
+        let mut result = teo_result::Error::new(message);
+        result.meta_map = value.meta_map.clone();
+        result
     }
 }
 
