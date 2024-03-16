8ba9e70bb69f7d6f21f9df4864c52559612c33b6
Victor Teo <victor.teo@fillmula.com>
2024-03-16 23:02:05 +0800

    update message

--- src/stdlib/identity/mod.rs
+++ src/stdlib/identity/mod.rs
@@ -171,7 +171,7 @@ pub(super) fn load_identity_library(std_namespace: &mut Namespace) {
         let self_pipeline_ctx = pipeline::Ctx::new(Object::from(&object), object.clone(), path![], CODE_NAME | CODE_AMOUNT | CODE_POSITION, req_ctx.transaction_ctx(), Some(req_ctx.clone()));
         if let Some(validator) = model.data.get("identity:validateAccount") {
             let validator = validator.as_pipeline().unwrap();
-            let _ = self_pipeline_ctx.run_pipeline_into_path_value_error(validator).await?;
+            let _ = self_pipeline_ctx.run_pipeline(validator).await?;
         }
         let Some(token_issuer) = model.data.get("identity:tokenIssuer") else {
             return Err(Error::internal_server_error_message_only("missing identity token issuer"));
--- src/stdlib/pipeline_items/debug.rs
+++ src/stdlib/pipeline_items/debug.rs
@@ -1,6 +1,6 @@
 use crate::namespace::Namespace;
 use crate::arguments::Arguments;
-use crate::pipeline::Ctx;
+use crate::pipeline::{Ctx, Pipeline};
 
 pub(in crate::stdlib) fn load_debug_items(namespace: &mut Namespace) {
 
@@ -9,5 +9,29 @@ pub(in crate::stdlib) fn load_debug_items(namespace: &mut Namespace) {
         println!("{}{}", if let Some(label) = label { format!("{}: ", label) } else { "".to_owned() }, ctx.value());
         Ok(ctx.value().clone())
     });
+
+    namespace.define_pipeline_item("message", |args: Arguments, ctx: Ctx| async move {
+        let pipeline: &Pipeline = args.get("pipeline")?;
+        let message: &str = args.get("message")?;
+        let code: Option<i32> = args.get_optional("code")?;
+        match ctx.run_pipeline(pipeline).await {
+            Ok(result) => Ok(result),
+            Err(mut error) => {
+                if let Some(errors) = &error.errors {
+                    error.errors = Some(errors.iter().map(|(k, v)| (k.to_owned(), message.to_owned())).collect());
+                    Err(error)
+                } else {
+                    error.message = message.to_owned();
+                    error.code = if let Some(code) = code {
+                        Some(code as u16)
+                    } else {
+                        None
+                    };
+                    Err(error)
+                }
+            }
+        }
+    });
+
 
 }
