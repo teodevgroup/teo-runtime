b6d8ef92e3051224bded6fa1356fe5cae96f9a23
Victor Teo <victor.teo@fillmula.com>
2024-03-22 04:49:02 +0800

    all and any

--- src/stdlib/pipeline_items/logical.rs
+++ src/stdlib/pipeline_items/logical.rs
@@ -4,6 +4,7 @@ use crate::namespace::Namespace;
 use crate::pipeline::Ctx;
 use crate::pipeline::pipeline::Pipeline;
 use teo_result::{Result, ResultExt};
+use tokio::net::unix::pipe::pipe;
 use crate::action::Action;
 use crate::value::Value;
 
@@ -67,13 +68,21 @@ pub(in crate::stdlib) fn load_pipeline_logical_items(namespace: &mut Namespace) 
     });
 
     namespace.define_pipeline_item("all", |args: Arguments, ctx: Ctx| async move {
-        // let pipelines: Vec<&Pipeline> = args.get("pipeline").error_message_prefixed("all")?;
-        // Ok(ctx.value().clone())
+        let pipelines: Vec<&Pipeline> = args.get("pipeline").error_message_prefixed("all")?;
+        for pipeline in pipelines {
+            ctx.run_pipeline_ignore_return_value(pipeline).await?;
+        }
+        Ok(ctx.value().clone())
     });
 
     namespace.define_pipeline_item("any", |args: Arguments, ctx: Ctx| async move {
-        // let pipelines: Vec<&Pipeline> = args.get("pipelines").error_message_prefixed("any")?;
-        // Ok(ctx.value().clone())
+        let pipelines: Vec<&Pipeline> = args.get("pipeline").error_message_prefixed("all")?;
+        for pipeline in pipelines {
+            if let Ok(_) = ctx.run_pipeline_ignore_return_value(pipeline).await {
+                return Ok(ctx.value().clone());
+            };
+        }
+        Err(Error::new("any: none of the conditions succeed"))
     });
 
     namespace.define_pipeline_item("when", |args: Arguments, ctx: Ctx| async move {
