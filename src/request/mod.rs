ff7b4f92b7ca9135d1cb9f2512d75c839052f1b0
Victor Teo <victor.teo@fillmula.com>
2024-07-10 14:07:11 +0800

    remove obsolete request items

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,9 +1,9 @@
-pub mod header;
 pub mod request;
 pub mod ctx;
 pub mod local;
 pub mod r#match;
-pub mod cookie;
 
+pub use actix_http::header::HeaderMap;
+pub use actix_web::cookie::Cookie;
 pub use request::Request;
 pub use ctx::Ctx;
