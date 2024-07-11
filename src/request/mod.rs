5e007e200d35fcfbeac7c0f1e59e00ee79409683
Victor Teo <victor.teo@fillmula.com>
2024-07-12 00:51:37 +0800

    fix

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -4,6 +4,6 @@ pub mod local;
 pub mod r#match;
 
 pub use actix_http::header::HeaderMap;
-pub use actix_web::cookie::{Cookie, Expiration};
+pub use actix_web::cookie::{Cookie, Expiration, SameSite};
 pub use request::Request;
 pub use ctx::Ctx;
