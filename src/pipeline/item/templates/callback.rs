a5ef6e0a4229736ec5e35d331a67fe7b9caf598f
Victor Teo <victor.teo@fillmula.com>
2024-11-29 21:33:28 +0800

    update callback to accept value

--- src/pipeline/item/templates/callback.rs
+++ src/pipeline/item/templates/callback.rs
@@ -1,8 +1,9 @@
 use std::future::Future;
 use futures_util::future::BoxFuture;
-use teo_result::Result;
+use teo_result::{Error, Result};
 use crate::pipeline::Ctx;
 use crate::pipeline::ctx::extract::ExtractFromPipelineCtx;
+use crate::Value;
 
 pub enum CallbackResult {
     Result(Result<()>)
@@ -24,39 +25,48 @@ pub trait Callback<A, O: Into<CallbackResult>>: Send + Sync + Clone + 'static {
     fn call(&self, ctx: Ctx) -> BoxFuture<'static, O>;
 }
 
+impl<O, F, Fut> Callback<(), O> for F where
+    F: Fn() -> Fut + Sync + Send + Clone + 'static,
+    O: Into<CallbackResult> + Send + Sync,
+    Fut: Future<Output = O> + Send + 'static {
+    fn call(&self, _: Ctx) -> BoxFuture<'static, O> {
+        Box::pin(self())
+    }
+}
+
 impl<A0, O, F, Fut> Callback<(A0,), O> for F where
-    A0: ExtractFromPipelineCtx + Send + Sync,
+    A0: TryFrom<Value, Error=Error> + Send + Sync,
     F: Fn(A0) -> Fut + Sync + Send + Clone + 'static,
     O: Into<CallbackResult> + Send + Sync,
     Fut: Future<Output = O> + Send + 'static {
     fn call(&self, ctx: Ctx) -> BoxFuture<'static, O> {
-        let value: A0 = ExtractFromPipelineCtx::extract(&ctx);
+        let value: A0 = ctx.value().clone().try_into().unwrap();
         Box::pin(self(value))
     }
 }
 
 impl<A0, A1, O, F, Fut> Callback<(A0, A1), O> for F where
-    A0: ExtractFromPipelineCtx + Send + Sync,
+    A0: TryFrom<Value, Error=Error> + Send + Sync,
     A1: ExtractFromPipelineCtx + Send + Sync,
     F: Fn(A0, A1) -> Fut + Sync + Send + Clone + 'static,
     O: Into<CallbackResult> + Send + Sync,
     Fut: Future<Output = O> + Send + 'static {
     fn call(&self, ctx: Ctx) -> BoxFuture<'static, O> {
-        let value: A0 = ExtractFromPipelineCtx::extract(&ctx);
+        let value: A0 = ctx.value().clone().try_into().unwrap();
         let arg1: A1 = ExtractFromPipelineCtx::extract(&ctx);
         Box::pin(self(value, arg1))
     }
 }
 
 impl<A0, A1, A2, O, F, Fut> Callback<(A0, A1, A2), O> for F where
-    A0: ExtractFromPipelineCtx + Send + Sync,
+    A0: TryFrom<Value, Error=Error> + Send + Sync,
     A1: ExtractFromPipelineCtx + Send + Sync,
     A2: ExtractFromPipelineCtx + Send + Sync,
     F: Fn(A0, A1, A2) -> Fut + Sync + Send + Clone + 'static,
     O: Into<CallbackResult> + Send + Sync,
     Fut: Future<Output = O> + Send + 'static {
     fn call(&self, ctx: Ctx) -> BoxFuture<'static, O> {
-        let value: A0 = ExtractFromPipelineCtx::extract(&ctx);
+        let value: A0 = ctx.value().clone().try_into().unwrap();
         let arg1: A1 = ExtractFromPipelineCtx::extract(&ctx);
         let arg2: A2 = ExtractFromPipelineCtx::extract(&ctx);
         Box::pin(self(value, arg1, arg2))
@@ -64,7 +74,7 @@ impl<A0, A1, A2, O, F, Fut> Callback<(A0, A1, A2), O> for F where
 }
 
 impl<A0, A1, A2, A3, O, F, Fut> Callback<(A0, A1, A2, A3), O> for F where
-    A0: ExtractFromPipelineCtx + Send + Sync,
+    A0: TryFrom<Value, Error=Error> + Send + Sync,
     A1: ExtractFromPipelineCtx + Send + Sync,
     A2: ExtractFromPipelineCtx + Send + Sync,
     A3: ExtractFromPipelineCtx + Send + Sync,
@@ -72,10 +82,29 @@ impl<A0, A1, A2, A3, O, F, Fut> Callback<(A0, A1, A2, A3), O> for F where
     O: Into<CallbackResult> + Send + Sync,
     Fut: Future<Output = O> + Send + 'static {
     fn call(&self, ctx: Ctx) -> BoxFuture<'static, O> {
-        let value: A0 = ExtractFromPipelineCtx::extract(&ctx);
+        let value: A0 = ctx.value().clone().try_into().unwrap();
         let arg1: A1 = ExtractFromPipelineCtx::extract(&ctx);
         let arg2: A2 = ExtractFromPipelineCtx::extract(&ctx);
         let arg3: A3 = ExtractFromPipelineCtx::extract(&ctx);
         Box::pin(self(value, arg1, arg2, arg3))
+    }
+}
+
+impl<A0, A1, A2, A3, A4, O, F, Fut> Callback<(A0, A1, A2, A3, A4), O> for F where
+    A0: TryFrom<Value, Error=Error> + Send + Sync,
+    A1: ExtractFromPipelineCtx + Send + Sync,
+    A2: ExtractFromPipelineCtx + Send + Sync,
+    A3: ExtractFromPipelineCtx + Send + Sync,
+    A4: ExtractFromPipelineCtx + Send + Sync,
+    F: Fn(A0, A1, A2, A3, A4) -> Fut + Sync + Send + Clone + 'static,
+    O: Into<CallbackResult> + Send + Sync,
+    Fut: Future<Output = O> + Send + 'static {
+    fn call(&self, ctx: Ctx) -> BoxFuture<'static, O> {
+        let value: A0 = ctx.value().clone().try_into().unwrap();
+        let arg1: A1 = ExtractFromPipelineCtx::extract(&ctx);
+        let arg2: A2 = ExtractFromPipelineCtx::extract(&ctx);
+        let arg3: A3 = ExtractFromPipelineCtx::extract(&ctx);
+        let arg4: A4 = ExtractFromPipelineCtx::extract(&ctx);
+        Box::pin(self(value, arg1, arg2, arg3, arg4))
     }
 }
